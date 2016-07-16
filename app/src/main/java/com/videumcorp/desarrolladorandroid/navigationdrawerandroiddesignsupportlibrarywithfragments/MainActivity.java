8d3def59de0c572c4b94312c9a1d666ace3fb471
Sebastian Ariel Bevacqua <sebabeva@gmail.com>
2016-07-16 01:29:58 -0300

    :sparkles: Butter Knife view binding implemented in MainActivity. Updated NavigationDrawer setup.

--- app/src/main/java/com/videumcorp/desarrolladorandroid/navigationdrawerandroiddesignsupportlibrarywithfragments/MainActivity.java
+++ app/src/main/java/com/videumcorp/desarrolladorandroid/navigationdrawerandroiddesignsupportlibrarywithfragments/MainActivity.java
@@ -7,7 +7,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -17,36 +17,37 @@ import android.widget.Toast;
 import com.videumcorp.desarrolladorandroid.navigationdrawerandroiddesignsupportlibrarywithfragments.Fragments.InboxFragment;
 import com.videumcorp.desarrolladorandroid.navigationdrawerandroiddesignsupportlibrarywithfragments.Fragments.StarredFragment;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
-    DrawerLayout drawerLayout;
-    Toolbar toolbar;
-    ActionBar actionBar;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    @BindView(R.id.toolbar)
+    private Toolbar toolbar;
+    @BindView(R.id.navigation_drawer_layout)
+    private DrawerLayout drawerLayout;
+    @BindView(R.id.navigation_view)
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        // Set a Toolbar to replace the ActionBar.
         setSupportActionBar(toolbar);
 
-        actionBar = getSupportActionBar();
-        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_white_24dp);
-        actionBar.setDisplayHomeAsUpEnabled(true);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
 
-        drawerLayout = (DrawerLayout) findViewById(R.id.navigation_drawer_layout);
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
-        if (navigationView != null) {
-            setupNavigationDrawerContent(navigationView);
-        }
-
-        setupNavigationDrawerContent(navigationView);
+        navigationView.setNavigationItemSelectedListener(this);
 
         //First fragment
         setFragment(0);
-
     }
 
     @Override
@@ -66,49 +67,7 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private void setupNavigationDrawerContent(NavigationView navigationView) {
-        navigationView.setNavigationItemSelectedListener(
-                new NavigationView.OnNavigationItemSelectedListener() {
-                    @Override
-                    public boolean onNavigationItemSelected(MenuItem menuItem) {
-                        switch (menuItem.getItemId()) {
-                            case R.id.item_navigation_drawer_inbox:
-                                menuItem.setChecked(true);
-                                setFragment(0);
-                                drawerLayout.closeDrawer(GravityCompat.START);
-                                return true;
-                            case R.id.item_navigation_drawer_starred:
-                                menuItem.setChecked(true);
-                                setFragment(1);
-                                drawerLayout.closeDrawer(GravityCompat.START);
-                                return true;
-                            case R.id.item_navigation_drawer_sent_mail:
-                                menuItem.setChecked(true);
-                                drawerLayout.closeDrawer(GravityCompat.START);
-                                return true;
-                            case R.id.item_navigation_drawer_drafts:
-                                menuItem.setChecked(true);
-                                drawerLayout.closeDrawer(GravityCompat.START);
-                                return true;
-                            case R.id.item_navigation_drawer_settings:
-                                menuItem.setChecked(true);
-                                Toast.makeText(MainActivity.this, "Launching " + menuItem.getTitle().toString(), Toast.LENGTH_SHORT).show();
-                                drawerLayout.closeDrawer(GravityCompat.START);
-                                Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
-                                startActivity(intent);
-                                return true;
-                            case R.id.item_navigation_drawer_help_and_feedback:
-                                menuItem.setChecked(true);
-                                Toast.makeText(MainActivity.this, menuItem.getTitle().toString(), Toast.LENGTH_SHORT).show();
-                                drawerLayout.closeDrawer(GravityCompat.START);
-                                return true;
-                        }
-                        return true;
-                    }
-                });
-    }
-
-    public void setFragment(int position) {
+    private void setFragment(int position) {
         FragmentManager fragmentManager;
         FragmentTransaction fragmentTransaction;
         switch (position) {
@@ -127,6 +86,39 @@ public class MainActivity extends AppCompatActivity {
                 fragmentTransaction.commit();
                 break;
         }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.item_navigation_drawer_inbox:
+                menuItem.setChecked(true);
+                setFragment(0);
+                break;
+            case R.id.item_navigation_drawer_starred:
+                menuItem.setChecked(true);
+                setFragment(1);
+                break;
+            case R.id.item_navigation_drawer_sent_mail:
+                menuItem.setChecked(true);
+                break;
+            case R.id.item_navigation_drawer_drafts:
+                menuItem.setChecked(true);
+                break;
+            case R.id.item_navigation_drawer_settings:
+                menuItem.setChecked(true);
+                Toast.makeText(MainActivity.this, "Launching " + menuItem.getTitle().toString(), Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.item_navigation_drawer_help_and_feedback:
+                menuItem.setChecked(true);
+                Toast.makeText(MainActivity.this, menuItem.getTitle().toString(), Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
 
