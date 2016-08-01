7cc8b196009be3569224073b1df34f107d417e29
Sebastian Ariel Bevacqua <sebabeva@gmail.com>
2016-08-01 01:06:42 -0300

    :wrench: :construction: Code refactoring for the navigation drawer.

--- app/src/main/java/com/videumcorp/desarrolladorandroid/navigationdrawerandroiddesignsupportlibrarywithfragments/activities/MainActivity.java
+++ app/src/main/java/com/videumcorp/desarrolladorandroid/navigationdrawerandroiddesignsupportlibrarywithfragments/activities/MainActivity.java
@@ -3,17 +3,17 @@ package com.videumcorp.desarrolladorandroid.navigationdrawerandroiddesignsupport
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.util.SparseArray;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.videumcorp.desarrolladorandroid.navigationdrawerandroiddesignsupportlibrarywithfragments.R;
-import com.videumcorp.desarrolladorandroid.navigationdrawerandroiddesignsupportlibrarywithfragments.fragments.BaseFragment;
 import com.videumcorp.desarrolladorandroid.navigationdrawerandroiddesignsupportlibrarywithfragments.fragments.InboxFragment;
 import com.videumcorp.desarrolladorandroid.navigationdrawerandroiddesignsupportlibrarywithfragments.fragments.StarredFragment;
 
@@ -38,7 +38,7 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
         navigationView.setNavigationItemSelectedListener(this);
 
         //First fragment
-        setFragment(0);
+        onNavigationItemSelected(navigationView.getMenu().findItem(R.id.item_navigation_drawer_inbox));
     }
 
     @Override
@@ -63,58 +63,73 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
         return super.onOptionsItemSelected(item);
     }
 
-    private void setFragment(int position) {
-        FragmentManager fragmentManager;
-        FragmentTransaction fragmentTransaction;
-        switch (position) {
-            case 0:
-                fragmentManager = getSupportFragmentManager();
-                fragmentTransaction = fragmentManager.beginTransaction();
-                BaseFragment inboxFragment = new InboxFragment();
-                fragmentTransaction.replace(R.id.fragment, inboxFragment);
-                fragmentTransaction.commit();
-                break;
-            case 1:
-                fragmentManager = getSupportFragmentManager();
-                fragmentTransaction = fragmentManager.beginTransaction();
-                BaseFragment starredFragment = new StarredFragment();
-                fragmentTransaction.replace(R.id.fragment, starredFragment);
-                fragmentTransaction.commit();
-                break;
-        }
-    }
+    // https://github.com/samerzmd/Navigation-Drawer-set-as-always-opened-on-tablets
 
+    // https://github.com/Suleiman19/Android-Material-Design-for-pre-Lollipop/blob/master/MaterialSample/app/src/main/java/com/suleiman/material/activities/NavDrawerActivity.java
     @Override
     public boolean onNavigationItemSelected(MenuItem menuItem) {
-        switch (menuItem.getItemId()) {
+        int menuItemId = menuItem.getItemId();
+        Fragment fragment = getFragment(menuItem);
+        if (fragment != null) {
+            navigationView.setCheckedItem(menuItemId);
+
+            // Highlight the selected item, update the title, and close the drawer
+            menuItem.setChecked(true);
+            setTitle(menuItem.getTitle());
+            drawerLayout.closeDrawers();
+
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.fragment, fragment).commit();
+        }
+
+        return true;
+    }
+
+// https://guides.codepath.com/android/Fragment-Navigation-Drawer#making-status-bar-translucent
+    private Fragment getFragment(MenuItem menuItem) {
+        Class fragmentClass = null;
+        Fragment fragment = null;
+        int menuItemId = menuItem.getItemId();
+
+        switch (menuItemId) {
             case R.id.item_navigation_drawer_inbox:
-                menuItem.setChecked(true);
-                setFragment(0);
+                fragmentClass = InboxFragment.class;
                 break;
             case R.id.item_navigation_drawer_starred:
-                menuItem.setChecked(true);
-                setFragment(1);
+                fragmentClass = StarredFragment.class;
                 break;
             case R.id.item_navigation_drawer_sent_mail:
-                menuItem.setChecked(true);
                 break;
             case R.id.item_navigation_drawer_drafts:
-                menuItem.setChecked(true);
                 break;
             case R.id.item_navigation_drawer_settings:
-                menuItem.setChecked(true);
                 Toast.makeText(MainActivity.this, "Launching " + menuItem.getTitle().toString(), Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
                 startActivity(intent);
                 break;
             case R.id.item_navigation_drawer_help_and_feedback:
-                menuItem.setChecked(true);
                 Toast.makeText(MainActivity.this, menuItem.getTitle().toString(), Toast.LENGTH_SHORT).show();
                 break;
         }
 
-        drawerLayout.closeDrawer(GravityCompat.START);
-        return true;
+        if(fragmentClass != null) {
+            try {
+                fragment = (Fragment) fragmentClass.newInstance();
+            } catch (InstantiationException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return fragment;
     }
+
+    SparseArray<Class> map = new SparseArray<Class>() {{
+        put(R.id.item_navigation_drawer_inbox, InboxFragment.class);
+        put(R.id.item_navigation_drawer_starred, StarredFragment.class);
+    }};
+
+
 }
 
